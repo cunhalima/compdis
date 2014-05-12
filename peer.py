cf280cbc6165d7bf4b4fdfb87e44c4c5b8ea11f9
cunhalima <reimanncl@gmail.com>
2014-05-12 11:16:57 -0300

    111
    111

---
+++ peer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import xmlrpclib
+import md5
+import SimpleXMLRPCServer
+import thread
+import threading
+import sys
+import string
+import time
+import copy
+import itertools
+
+# Ver PyCSP
+
+known_peers = []		# lista de peers conhecidos
+
+# A cada 30 ms, chamar getPeers(String)
+
+
+#class Functions:
+#    def getPeers(self, tell_peers):
+#        return known_peers
+
+a = "aaaa"
+a = 12
